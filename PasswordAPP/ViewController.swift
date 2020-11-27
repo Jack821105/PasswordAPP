59c188a1260a283c7866a4de401c1581f52a017d
Jack <git clone https://j4688821105@bitbucket.org/etcapp/jack.git>
2020-11-27 16:25:05 +0800

    Initial Commit

---
+++ PasswordAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PasswordAPP
+//
+//  Created by 許自翔 on 2020/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
